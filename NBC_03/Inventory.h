3755050f9ce82cb7ef93b974ee1f70ad32e609a9
Yoongisang <sawsd@naver.com>
2025-12-18 20:49:56 +0900

    정리 및 주석 추가

--- NBC_03/Inventory.h
+++ NBC_03/Inventory.h
@@ -4,7 +4,7 @@
 #include "ItemBase.h"
 using namespace std;
 
-// 포인터용 비교 함수
+// 포인터용 비교 함수: pItems_가 포인터 배열이므로 매개변수도 포인터로 받음
 bool compareItemsByPrice(ItemBase* a, ItemBase* b) {
 	return a->GetPrice() < b->GetPrice();
 }
@@ -37,6 +37,8 @@ public:
 };
 
 
+
+
 template<typename T>
 inline Inventory<T>::Inventory(int capacity) : capacity_(capacity), size_(0)
 {
@@ -45,16 +47,18 @@ inline Inventory<T>::Inventory(int capacity) : capacity_(capacity), size_(0)
 	{
 		capacity_ = 1;
 	}
-	// 받은 용량대로 아이템 동적배열
+	// 받은 용량만큼 배열 동적 할당
 	pItems_ = new T[capacity_];
 }
 
 template<typename T>
 inline Inventory<T>::Inventory(const Inventory<T>& other)
 {
+	// 복사생성자로 다른 객체의 값들을 복사하기위해 capacity, size값 복사 new T[capacity]로 동적 할당 
 	capacity_ = other.capacity_;
 	size_ = other.size_;
 	pItems_ = new T[capacity_];
+	//other의 pItems_의 값들을 복사(깊은복사)
 	for (int i = 0; i < size_; ++i)
 	{
 		pItems_[i] = other.pItems_[i];
@@ -82,19 +86,21 @@ inline void Inventory<T>::AddItem(const T& item)
 	if (size_ >= capacity_)
 	{
 		cout << "인벤토리가 꽉 차서 확장합니다." << '\n';
+		// 확장하기 위해 새 공간을 기존 공간 * 2로 설정 그 크기만큼 동적할당한 임시포인터 생성
 		int newCapacity = capacity_ * 2;
 		T* temp = new T[newCapacity];
-
+		// 임시 포인터에 기존의 값들을 복사
 		for (int i = 0; i < size_; i++)
 		{
 			temp[i] = pItems_[i];
 		}
+		// 기존 포인터 해제
 		delete[] pItems_;
-
+		// 기존 포인터에 임시 포인터 주소 대입, capacity_도 새 크기로 갱신
 		pItems_ = temp;
 		capacity_ = newCapacity;
 	}
-
+	// 추가하려던 새 item을 추가 후 size++
 	pItems_[size_] = item;
 	size_++;
 }
@@ -108,7 +114,7 @@ inline void Inventory<T>::RemoveLastItem()
 		cout << "인벤토리가 비어있습니다." << '\n';
 		return;
 	}
-
+	// 배열의 마지막 item 삭제, size--
 	pItems_[size_-1] = T();
 	size_--;
 }
@@ -116,20 +122,20 @@ inline void Inventory<T>::RemoveLastItem()
 template<typename T>
 inline void Inventory<T>::Resize(int newCapacity)
 {
+	// 새로 설정하는 공간이 0보다 작다면 생성자 규칙과 같게 1로 설정
 	if (newCapacity <= 0)
 	{
 		newCapacity = 1;
 	}
-
+	// 재설정된 capacity 크기만큼 동적 할당
 	T* temp = new T[newCapacity];
-	// 용량과 기존 size_중 작은 값만큼 복사
+	// 새로 설정된 용량과 기존 size_중 작은 값만큼 copySize로 설정한 다음 아이템 데이터 복사(깊은 복사)
 	int copySize = min(newCapacity, size_);
-
 	for (int i = 0; i < copySize; i++)
 	{
 		temp[i] = pItems_[i];
 	}
-
+	// 기존 포인터 해제 및 임시포인터의 주소를 가리키도록 설정 capcity_, size_ 값 재설정
 	delete[] pItems_;
 	pItems_ = temp;
 	capacity_ = newCapacity;
@@ -139,11 +145,13 @@ inline void Inventory<T>::Resize(int newCapacity)
 template<typename T>
 inline void Inventory<T>::SortItems()
 {
+	// 아이템이 1개 이하면 정렬 필요 X
 	if (size_ <= 1)
 	{
 		return;
 	}
-
+	// sort(시작, 끝): 끝은 마지막 요소 다음을 가리킴
+	// pItems_ + size_ = 배열 끝 다음 위치	
 	sort(pItems_, pItems_ + size_, compareItemsByPrice);
 	
 }
@@ -151,16 +159,18 @@ inline void Inventory<T>::SortItems()
 template<typename T>
 inline void Inventory<T>::Assign(const Inventory<T>& other)
 {
+	// 자기 자신에게 대입하는 경우 무시
 	if (this == &other)
 	{
 		return;
 	}
+	// 기존 포인터 해제
 	delete[] pItems_;
-
+	// capacity_, size_ 재설정 포인터 새로 동적할당
 	capacity_ = other.capacity_;
 	size_ = other.size_;
 	pItems_ = new T[capacity_];
-
+	// 아이템 데이터 깊은복사 
 	for (int i = 0; i < size_; ++i) {
 		pItems_[i] = other.pItems_[i];
 	}
@@ -178,7 +188,8 @@ inline void Inventory<T>::PrintAllItems() const
 	}
 
 	for (int i = 0; i < size_; i++)
-	{
+	{	
+		// pItems_가 담고있는 클래스 객체의 PrintInfo()함수 호출
 		pItems_[i]->PrintInfo();
 	}
 }
--- NBC_03/ItemBase.h
+++ NBC_03/ItemBase.h
@@ -7,12 +7,14 @@ protected:
 	std::string name_;
 	int price_;
 public:
+	// 생성과 동시에 아이템 이름, 가격 할당
 	ItemBase(std::string name, int price) : name_(name), price_(price) {}
+	// 아이템 정보 출력
 	void PrintInfo() const;
 	
-
+	// 게터 함수(이름, 가격)
 	std::string GetName() const { return name_; }
 	int GetPrice() const { return price_; }
-
+	// 기본 소멸자 상속 대비 virtual사용
 	virtual ~ItemBase() = default;
 };
